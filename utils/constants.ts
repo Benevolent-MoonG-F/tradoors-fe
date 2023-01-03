2ebc24f628359913b1d1d6fb6cc7f1d175e95ed9
Godswill Adigwe <gcadigwe@gmail.com>
2023-01-03 15:33:34 +0100

    update address

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,4 +5,4 @@ export const NFT_ADDRESS =
 //   "juno1uhjfy58c5mtdnqkk5f8403uu3t6y4j0qdlntfttj9kvakcaqz3gsh2hdsg";
 
 export const DR_ADDRESS =
-  "juno1ssd4pcjzsyv5mku4pur7yf82nxpql8tgaswqlpylhyjqwmqed50q7qzuel";
+  "juno1pdaquxxwxpygqaey0e0qx3w3a5x5tynl0txfx0dsmg5frvt6pugqm3jg2a";
