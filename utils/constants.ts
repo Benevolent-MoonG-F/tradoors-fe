25b32d3379cefe9dc59edf6bf815c027773f203c
Godswill Adigwe <gcadigwe@gmail.com>
2023-01-12 23:13:53 +0100

    add docker file

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,8 @@
 export const NFT_ADDRESS =
-  "juno1seq2nkqjmw5da9c5vkr475yefzm6qa4sry5wflqwvqjsjp3kzr6qks3ehx";
+  "juno1qnmgent6fmw8rufz978rhmzfgmvme9gvyj0t7rkpffklvwr9tteste35lg";
 
 // export const DR_ADDRESS =
 //   "juno1uhjfy58c5mtdnqkk5f8403uu3t6y4j0qdlntfttj9kvakcaqz3gsh2hdsg";
 
 export const DR_ADDRESS =
-  "juno1l5fwr6hps85s07096vlxp8t86eyyy42m570tn5duyxws2aj04evs2d6ex4";
+  "juno1thg9yq9nywnwy22ksxzc65xk5nalz8r52wa96803dwrkv3ex9afq7nagqm";
